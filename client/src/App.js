827bda0cc853dc758f70d574995b531fc99877f5
Ivan Rendon <ivansito87@gmail.com>
2019-08-04 23:12:38 -0400

    adds components to index page

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,44 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import {BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom";
+// import Register from './components/Register/Register.jsx';
+// import Main from "./components/Main/Main";
+// import "./App.css";
+import Login from "./views/examples/Login"
+/*import "./assets/styles/nucleo/css/nucleo.css";
+import "./assets/css/argon-dashboard-react.css";
+import "./assets//font-awesome/css/font-awesome.css";*/
+import "./assets/vendor/nucleo/css/nucleo.css";
+import "./assets/vendor/@fortawesome/fontawesome-free/css/all.min.css";
+import "./assets/scss/argon-dashboard-react.scss";
+// import MultplayerMain2 from "./components/socketIOClient/Multiplayer-Main2";
+// import MainSocket from "./components/Main/MainSocket";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+/*
+function ProtectedRoute({component: Component, ...outerProps}) {
+  return <Route {...outerProps} render={props => {
+    let usrId = localStorage.getItem('id');
+    if (!usrId) {
+      return <Redirect to={`/login?referrer=${outerProps.location.pathname}`}/>;
+    } else {
+      return <Component {...props} />;
+    }
+  }}/>
 }
+*/
 
-export default App;
+export default function APP() {
+  return (
+    <React.Fragment>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Login}/>
+          {/*<Route exact path="/register" component={Register}/>
+          <ProtectedRoute exact path="/singleplayer" component={Main}/>
+          <ProtectedRoute exact path="/multiplayer/:roomId?" component={MultplayerMain2}/>
+          <ProtectedRoute path="/rooms/:id" component={MainSocket}/>
+          <Route exact path="*" component={Login}/>*/}
+        </Switch>
+      </Router>
+    </React.Fragment>
+  );
+}
